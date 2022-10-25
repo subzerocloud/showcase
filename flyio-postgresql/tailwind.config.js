0a7768d5b9d0e8c358012189719524bc4da2f15b
Ruslan Talpa <ruslan.talpa@subzero.cloud>
2022-10-25 12:42:47 +0300

    flyio postgresql example

---
+++ flyio-postgresql/tailwind.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+    "./public/**/*.html",
+  ],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+  theme: {},
+};
